0fc70a3622a3a176d76c06445abd0dd9b09cb288
aeremin <aeremin@maxus.ru>
2015-07-20 18:51:42 +0300

    +

--- grocer.rb
+++ grocer.rb
@@ -10,7 +10,7 @@ def apply_coupons(cart:[], coupons:[])
   # code here
   cart.class == Array ? con_cart = consolidate_cart(cart: cart) : con_cart = cart
   coupons.each_with_object(con_cart) do |item, res|
-    # here (k, v) are key and value of the con_cart's item which can be couponed
+    # here (k, v) are key and value of the con_cart's item, which can be couponed
 	k, v = res.detect { |k,v| k == item[:item] }
 	if k != nil && v[:count] >= item[:num] then
       times = v[:count] / item[:num]
